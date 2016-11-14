41b3ca78e9df2e7386baf0bbeec58b3e384e1e18
caoyong <caoyong@shopexunit.com>
2016-11-14 14:29:38 +0800

    初始化功能

---
+++ haiba/haiba/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  haiba
+//
+//  Created by caoyong on 16/11/14.
+//  Copyright © 2016年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
