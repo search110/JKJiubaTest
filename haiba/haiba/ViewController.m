913ef7e4d3ab19dc0cd17ad47a038c60b94c90d3
caoyong <1308210728@qq.com>
2018-08-03 16:08:47 +0800

    [代码功能]1添加描述

--- haiba/haiba/ViewController.m
+++ haiba/haiba/ViewController.m
@@ -26,6 +26,9 @@
 
 @property(copy,nonatomic)NSString * test10;
 
+//使用在merge 分支时候
+//第一次添加属性完成后 发现bug或者需要添加  revert 第一次的提交commit 第二次合并 修改完或者完善后 需要先再次revert 第一次revert的提交节点(因为如果不再次revert时候 第一次revert后默认改分支是永远被revert的)
+//第一次dev分支添加的属性
 @property(copy,nonatomic)NSString * dev;
 
 @property(copy,nonatomic)NSString * dev1;
